9fce9121925f5561d3599eb6a1d43ddb7c097ae5
Amber Koski <Koski_Kingdom@Ambers-MacBook-Pro.local>
2017-04-27 18:27:57 -0700

    Initial Commit

---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by Amber Koski on 4/27/17.
+//  Copyright © 2017 KoskiKingdom. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
